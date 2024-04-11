00f983f66229f7118b115ef464b3ab230bf2383b
Logical Guess <logicalguess@gmail.com>
2024-04-11 10:19:22 -0400

    single qubit app

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# code_plus
+### Setting up the code
+#### Clone repository
+Start by cloning the repository:
+```bash
+ git clone https://github.com/learnqc/code_plus
+```
+
+
+#### Code Setup
+
+1. Next, **create a virtual environment** where you can run the code.
+```bash
+python -m venv bqs-env
+```
+
+2. **Activate the new environment.**
+```bash
+source bqs-env/bin/activate
+```
+
+3. **Install the dependencies** for the repository in the virtual environment.
+```bash
+pip install -r requirements.txt
+```
+
+## Run the single-qubit UI application
+
+```bash
+cd src 
+ python -m panel serve experiments/single_qubit.py --show
+```
+
+![Single Qubit App](./assets/images/single_qubit_app.png)
---
+++ src/experiments/single_qubit.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python -m panel serve
+
+
+import pathlib
+import sys
+
+sys.path.append(str(pathlib.Path(__file__).parent.parent.resolve()))
+
+from components.single_qubit_component import state_table_to_string, SingleQubit, arg_gates, gates
+
+import panel as pn
+
+# # Explicitly set template and add some text to the header area
+template = pn.template.BootstrapTemplate(title='Building Quantum Software')
+
+template.header.append('### Single-Qubit Circuit')
+
+gate = pn.widgets.Select(name='Gate', options=[None] + [gate.upper() for gate in gates])
+arg = pn.widgets.NumberInput(name='Angle (in degrees)', disabled=True)
+go = pn.widgets.Button(name='Apply', button_type='primary')
+
+template.sidebar.extend([gate, arg, go])
+
+component = SingleQubit()
+
+
+def select_gate(v):
+    arg.disabled = gate.value is None or not (gate.value.lower() in arg_gates)
+
+
+def apply(v):
+    global out
+    if go.value is True and gate.value is not None:
+        # add_gate(qc, [], 0, gate.value.lower(), arg.value/180*pi if gate.value.lower() in arg_gates else None)
+        component.apply_gate(gate.value.lower(), arg.value)
+        gate.value = None
+        arg.value = None
+        arg.disabled = True
+        go.value = False
+
+        s = component.get_state()
+
+        # s = state_table_to_string(state)
+        qc_str = component.get_circuit()
+        out = f'Step {component.last_step()}\n-------\n\n{qc_str}\n\n{s}\n\n\n\n{out}'
+    # else:
+    #     s = component.get_state()
+    #     # s = state_table_to_string(state)
+    #     out = f'Initial state\n-------------\n\n{component.get_state()}'
+
+    return pn.pane.Str(out)
+
+component.reset()
+out = f'Initial state\n-------------\n\n{component.get_state()}'
+
+template.main.append(pn.Column(pn.bind(select_gate, gate), pn.bind(apply, go)))
+template.servable()
+
