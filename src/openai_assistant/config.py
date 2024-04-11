566add6ff8b3bffd15d76c8c8c9990b662a9ecc3
exponentis <ngonciul@yahoo.com>
2024-04-11 10:40:46 -0500

    clean up

--- src/openai_assistant/config.py
+++ src/openai_assistant/config.py
@@ -5,7 +5,6 @@ load_dotenv()
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 SINGLE_QUBIT_ASSISTANT_ID = os.getenv('SINGLE_QUBIT_ASSISTANT_ID')
-ASSISTANT_ID: os.getenv('ASSISTANT_ID')
 
 cfg = {
     'OPENAI_API_KEY': OPENAI_API_KEY,
